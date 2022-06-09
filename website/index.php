6c87a76d8c7af3d7bd8bc7a5fce27e1ef720b2be
VCTR07 <victor_emanuell@yahoo.com.br>
2022-06-09 18:28:45 -0300

    correções no index.php

--- website/index.php
+++ website/index.php
@@ -10,24 +10,23 @@
     $result = file_get_contents("http://node-container:9001/products");
     $products = json_decode($result);
   ?>
-  
-  <div class="container">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Produto</th>
-          <th>Preço</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach($products as $product): ?>
-          <tr>
-            <td><?php echo $product->name; ?></td>
-            <td><?php echo $product->price; ?></td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-  </div>
+
+
+   <table class="table">
+     <thead>
+       <tr>
+         <th>Produto</th>
+         <th>Preço</th>
+       </tr>
+     </thead>
+     <tbody>
+       <?php foreach($products as $product): ?>
+         <tr>
+           <td><?php echo $product->name; ?></td>
+           <td><?php echo $product->price; ?></td>
+         </tr>
+       <?php endforeach; ?>
+     </tbody>
+   </table>
 </body>
 </html>
